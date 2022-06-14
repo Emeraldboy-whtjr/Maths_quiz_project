2dc4d9a072ac2f7ebabb08e44e8b412889bd8f23
Emeraldboy-whtjr <82824596+Emeraldboy-whtjr@users.noreply.github.com>
2022-06-14 21:54:25 +0530

    Delete quiz_game_login.js

--- quiz_game_login.js
+++
@@ -1,9 +0,0 @@
-function adduser(){
-    player_1UserName = document.getElementById("player1_username").value;
-    player_2UserName = document.getElementById("player2_username").value;
-    
-    localStorage.setItem("player_1",player_1UserName);
-    localStorage.setItem("player_2",player_2UserName);
-
-    window.location = "quiz_game_page.html";
-}
